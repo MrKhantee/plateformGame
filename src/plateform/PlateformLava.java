0a886cf38b12ca5a898596ebbb6c329877dc1eb0
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-07-09 23:15:27 +0100

    guerre mort haine et destruction

--- src/plateform/PlateformLava.java
+++ src/plateform/PlateformLava.java
@@ -10,31 +10,10 @@ import model.Point;
 
 public class PlateformLava extends Plateform{
 	
-	private transient Vector<Point> lumieres = new Vector<Point>();
-	private transient int timeToGo = 0;
-	
 	public PlateformLava(float x, float y, float sizeX, float sizeY) {
 		super(x, y, sizeX, sizeY);
 		this.coefFrottement = Data.Flava;
 		lumieres = new Vector<Point>();
 		this.color = Color.red;
-	}
-	
-	public void draw(Graphics g){
-		if(timeToGo==0){
-			timeToGo=30;
-			lumieres = new Vector<Point>();
-			for(int i=0; i<1000; i++){
-				lumieres.add(new Point((float)(p.x*Data.ratioSpace+Math.random()*collisionBox.getWidth()*Data.ratioSpace), 
-					(float)(p.y*Data.ratioSpace+Math.random()*collisionBox.getHeight()*Data.ratioSpace)));
-			}
-		}
-		g.setColor(this.color);
-		g.fillRect(p.x*Data.ratioSpace, p.y*Data.ratioSpace, sizeX*Data.ratioSpace, sizeY*Data.ratioSpace);
-		g.setColor(Color.orange);
-		timeToGo--;
-		for(Point p : lumieres){
-			g.fillRect(p.x, p.y,4f, 4f);
-		}
 	}
 }
