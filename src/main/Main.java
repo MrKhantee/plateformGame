142fb937758e6f0e46ebc741447dbee3378b9c95
Spepsi <floriangardin@hotmail.fr>
2016-07-09 16:03:50 +0100

    commit vicié

---
+++ src/main/Main.java
@@ -0,0 +1,59 @@
+package main;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.Log;
+
+import model.Game;
+
+public class Main {
+	// A REGLER \\
+	public static float ratioSpace = 0.7f;
+	public static int framerate = 30;
+	public static int nDelay=5;
+	///////\\\\\\\\\
+	
+	public static float increment = 0.1f*30/Main.framerate;
+	public static boolean pleinEcran = true;
+	
+	public static void main(String[] args) {
+//		Log.setLogSystem(new NullLogSystem()); 
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+//		System.out.println(LWJGLUtil.getPlatformName());
+		int resolutionX;
+		int resolutionY;
+		if(pleinEcran){
+			resolutionX = (int)screenSize.getWidth();		
+			resolutionY = (int)screenSize.getHeight();
+		} else {
+			resolutionX = 1200;		
+			resolutionY = 800;
+		}
+		try {
+			Game game = new Game(resolutionX,resolutionY);
+			AppGameContainer app = new AppGameContainer( game );
+			game.app = app;
+			
+//			app.setDisplayMode(resolutionX, resolutionY,true);
+			app.setShowFPS(true);
+			app.setDisplayMode(resolutionX, resolutionY,pleinEcran);
+			app.setAlwaysRender(false);
+			app.setUpdateOnlyWhenVisible(false);
+			app.setClearEachFrame(true);
+			app.setVSync(true);
+			//app.setSmoothDeltas(true);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+
+}
---
+++ src/model/Game.java
@@ -0,0 +1,48 @@
+package model;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+	
+	
+	public static Game g;
+	public int resX,resY;
+	public AppGameContainer app;
+	public Game(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+		Game.g = this;
+	}
+
+	public Game(int resolutionX, int resolutionY) {
+		// TODO Auto-generated constructor stub
+		super("Vicier");
+		Game.g = this;
+		this.resX = resolutionX;
+		this.resY = resolutionY;
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
