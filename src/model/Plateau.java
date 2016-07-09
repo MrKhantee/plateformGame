4cff96443473cbd0896cc0e37b592b0863d5df62
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-07-09 23:05:14 +0100

    guerre mort haine et destruction

--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -28,7 +28,7 @@ public class Plateau implements java.io.Serializable{
 		this.players.add(new Player(Data.RADIUS_PLAYER, new Point(1820f,50f),1));
 		this.players.add(new Player(Data.RADIUS_PLAYER, new Point(100f,50f),2));
 		// Bords de la map
-		this.plateforms.addElement(new PlateformLava(0,Data.sizeYPlateau-10,Data.sizeXPlateau,400));
+		this.plateforms.addElement(new PlateformLava(0,Data.sizeYPlateau-10,Data.sizeXPlateau,40));
 		this.plateforms.addElement(new Plateform(0,-10,Data.sizeXPlateau,10));
 		this.plateforms.addElement(new Plateform(-10,0,10,Data.sizeYPlateau));
 		this.plateforms.addElement(new Plateform(Data.sizeXPlateau,0,10,Data.sizeYPlateau));
--- src/plateform/PlateformLava.java
+++ src/plateform/PlateformLava.java
@@ -1,22 +1,40 @@
 package plateform;
 
+import java.util.Vector;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 import model.Data;
+import model.Point;
 
 public class PlateformLava extends Plateform{
 	
+	private transient Vector<Point> lumieres;
+	private transient int timeToGo = 0;
 	
 	public PlateformLava(float x, float y, float sizeX, float sizeY) {
 		super(x, y, sizeX, sizeY);
 		this.coefFrottement = Data.Flava;
+		lumieres = new Vector<Point>();
 		this.color = Color.red;
 	}
 	
 	public void draw(Graphics g){
+		if(timeToGo==0){
+			timeToGo=30;
+			lumieres.clear();
+			for(int i=0; i<1000; i++){
+				lumieres.add(new Point((float)(p.x*Data.ratioSpace+Math.random()*collisionBox.getWidth()*Data.ratioSpace), 
+					(float)(p.y*Data.ratioSpace+Math.random()*collisionBox.getHeight()*Data.ratioSpace)));
+			}
+		}
 		g.setColor(this.color);
 		g.fillRect(p.x*Data.ratioSpace, p.y*Data.ratioSpace, sizeX*Data.ratioSpace, sizeY*Data.ratioSpace);
-		g.setColor(Color.yellow);
+		g.setColor(Color.orange);
+		timeToGo--;
+		for(Point p : lumieres){
+			g.fillRect(p.x, p.y,4f, 4f);
+		}
 	}
 }
