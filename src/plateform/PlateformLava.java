c63939d52d3a9916b9024418f56481625e81f27c
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-07-09 23:15:48 +0100

    guerre mort haine et destruction

--- src/plateform/PlateformLava.java
+++ src/plateform/PlateformLava.java
@@ -1,19 +1,15 @@
 package plateform;
 
-import java.util.Vector;
 
 import org.newdawn.slick.Color;
-import org.newdawn.slick.Graphics;
 
 import model.Data;
-import model.Point;
 
 public class PlateformLava extends Plateform{
 	
 	public PlateformLava(float x, float y, float sizeX, float sizeY) {
 		super(x, y, sizeX, sizeY);
 		this.coefFrottement = Data.Flava;
-		lumieres = new Vector<Point>();
 		this.color = Color.red;
 	}
 }
