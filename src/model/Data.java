4aae41fb435198e6038638f14cb7c97ea8cd077a
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-07-10 12:14:36 +0100

    guerre mort haine et destruction

--- src/model/Data.java
+++ src/model/Data.java
@@ -16,15 +16,15 @@ public class Data {
 	public static float damageLava = 0.5f;
 
 	public static float ACCContact = 65f;
-	public static float ACCLibre = 40f;
+	public static float ACCLibre = 30f;
 	public static float RADIUS_PLAYER  = 40f;
 	public static float DT = 10f/Main.framerate;
 	
 	// Frottements
 	public static float Flibre = 0.99f;
-	public static float Fplateforme = 0.80f;
+	public static float Fplateforme = 0.75f;
 	public static float Fglace = 0.95f;
-	public static float Flava = 0.5f;
+	public static float Flava = 0.4f;
 	
 	public static float speedJump = 220f;
 	public static float ratioVertical = 0.6f;
@@ -33,10 +33,11 @@ public class Data {
 
 	
 	// Weapon
+	public static float speedBullet = 300f;
 	public static float ACCBullet = 80f;
 	public static float Fbullet = 0.99f;
 	public static float bulletRadius = 6f;
 	public static float weaponRadius = 3f;
-	public static float chargeTime = 3f;
+	public static float chargeTime = 10f;
 	
 }
